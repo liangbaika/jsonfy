1a120fe71bae2cb1218b5a814b626c11092e8b34
liqiang <1144388620@qq.com>
2020-01-17 15:35:33 +0800

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jsonfy",
-    version="0.1.2",
+    version="0.2.0",
     keywords=["pip", "json", "jsonfy", "obj"],
     description="Jsonfy: Lightweight two-way binding of JSON and object without third-party dependency",
     long_description="Jsonfy: Lightweight two-way binding of JSON and object without third-party dependency, suppoert nesting,complex obj",
